cf77fc8927e2f8d04892b4070b8a56de88edfa0d
James Sheppard <jamessheppard@Verenas-MBP.public.ph.local>
2020-09-08 16:10:01 +0200

    finished original project, wrote some code for possible answers to stretch questions

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const chalk = require('chalk');
 
 //takes in inputs from user
 const chooseColor = {
-  hue: process.argv[3],
-  luminosity: process.argv[2],
+  hue: process.argv[2],
+  luminosity: process.argv[3],
 };
 const colorInput = randomColor(chooseColor);
 
-const hexDesign = `
+const hashDesign = `
 ###############################
 ###############################
 ###############################
@@ -19,9 +19,18 @@ const hexDesign = `
 ###############################
 ###############################`;
 
-//console.log(printHash(hashGenerator(31, 9)));
-console.log(chalk.hex(`${colorInput}`).bold(`${hexDesign}`));
+console.log(chalk.hex(`${colorInput}`).bold(`${hashDesign}`));
 
+/*if (process.argv[2] === 'ask') {
+  prompt('Which hue and/or shade would you like to pick?');
+} else {
+  console.log(chalk.hex(`${colorInput}`).bold(`${hashDesign}`));
+}*/
+
+/* if (process.argv[2] === 'ask') {
+  console.log('Which color would you like to choose?');
+} else {}
+*/
 /*
 //creates a 2D array of #'s and prints hex value in middle array
 function hashGenerator(width, height) {
