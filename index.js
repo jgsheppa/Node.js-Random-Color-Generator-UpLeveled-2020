f018cc189ae8474b0becc0a4d5fe8d382fbb9f08
James Sheppard <jamessheppard@Verenas-MBP.public.ph.local>
2020-09-08 14:45:08 +0200

    finished prototype for hash grid with hex value in the center

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const randomColor = require('randomcolor');
+const chalk = require('chalk');
+
+//takes in inputs from user
 const chooseColor = {
   hue: process.argv[3],
   luminosity: process.argv[2],
 };
 const colorInput = randomColor(chooseColor);
 
+const hexDesign = `
+###############################
+###############################
+###############################
+########                #######
+########    ${colorInput}     #######
+########                #######
+###############################
+###############################
+###############################`;
+
+//console.log(printHash(hashGenerator(31, 9)));
+console.log(chalk.hex(`${colorInput}`).bold(`${hexDesign}`));
+
+/*
+//creates a 2D array of #'s and prints hex value in middle array
 function hashGenerator(width, height) {
   let arr = [];
+  const hexValue = colorInput;
   for (let i = 0; i < height; i++) {
     arr[i] = [];
     for (let j = 0; j < width; j++) {
-      if (i < height / 2 || i > height / 2) {
+      if (i < height / 2 - 1 || i > height / 2) {
         arr[i][j] = '#';
-      } else if (i === height / 2) {
-        arr[i][j] = colorInput;
+      } else if (i === height % 2 && j === width % 2) {
+        arr[i][j] = hexValue;
       }
     }
   }
-  arr.join(' ');
   return arr;
 }
 
-//console.log(color);
-console.log('%c#', `color: ${colorInput}`);
+function printHash(hash) {
+  for (let i = 0; i < hash.length; i++) {
+    console.log(hash[i].join(''));
+  }
+}
+*/
